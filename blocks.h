7dc63255e0ed2720fe76270dbb0db63ab64e1b0b
i9p <55615254+i9p@users.noreply.github.com>
2021-12-18 17:03:02 +1100

    comment

--- blocks.h
+++ blocks.h
@@ -1,21 +1,17 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+/* blocks */
 static const Block blocks[] = {
-	/*Icon*/ /*Command*/            /*Update Interval*/ /*Update Signal*/
-	{"",     "status-temp",         1,                  7},
-	{"",     "status-cpu",          2,                  6},
-	{"",     "status-filesystem",   60,                 5},
-	{"",     "status-memory",       5,                  4},
-
-	{"",     "status-delim",        0,                  0},
-
-	{"",     "status-volume",       0,                  3},
-
-	{"",     "status-delim",        0,                  0},
-
-	{"",     "status-updates",      1800,               2},
-	{"",     "status-time",         5,                  1},
+	/* icon   command                update  signal */
+	{ "",     "status-temp",         1,      7 },
+	{ "",     "status-cpu",          2,      6 },
+	{ "",     "status-filesystem",   60,     5 },
+	{ "",     "status-memory",       5,      4 },
+	{ "",     "status-delim",        0,      0 },
+	{ "",     "status-volume",       0,      3 },
+	{ "",     "status-delim",        0,      0 },
+	{ "",     "status-updates",      1800,   2 },
+	{ "",     "status-time",         5,      1 },
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "\0";
+/* delimiter */
+static char delim[] = "\0"; /* NULL ('\0') means no delimiter */
 static unsigned int delimLen = 0;
